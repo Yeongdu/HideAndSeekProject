61e9e625cac6fe83465c4a0060996f633bf5ded9
yeajizy <116059892+yeajizy@users.noreply.github.com>
2022-12-01 19:32:44 +0900

    221201_join,login.jsp 작성 및 UserDAO,UserDAOImpl,UserController 진행 중

--- src/main/java/com/spring/controller/UserController.java
+++ src/main/java/com/spring/controller/UserController.java
@@ -1,8 +1,28 @@
 package com.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.model.UserDTO;
+import com.spring.service.UserDAO;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserDAO dao;
+	
+	@RequestMapping("user_login.do")
+	public String list() {
+		
+	return "user/login";
+		
+	}
+	
+	
 
 }
--- src/main/java/com/spring/service/UserDAO.java
+++ src/main/java/com/spring/service/UserDAO.java
@@ -1,6 +1,14 @@
 package com.spring.service;
 
+import java.util.List;
+
+import com.spring.model.UserDTO;
+
 public interface UserDAO {
+	
+	int insertUser(UserDTO userDTO);
+	
+	 List<UserDTO> getLoginList();
 	
 
 }
