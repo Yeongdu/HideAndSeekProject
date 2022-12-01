5740e4ee6a1fa071017f9bbbf6e001613ebc9f3d
leehyunji1107 <waffles03@naver.com>
2022-12-01 14:39:20 +0900

    Revert "Merge pull request #6 from Yeongdu/KYJ"
    This reverts commit b406f66f51964038653b2b636db58bfe85bde26c, reversing
    changes made to 151d6a46a3756ed81a964abb8700979f25127603.

--- src/main/java/com/spring/service/UserDAO.java
+++ src/main/java/com/spring/service/UserDAO.java
@@ -1,9 +1,5 @@
 package com.spring.service;
 
-import com.spring.model.UserDTO;
-
 public interface UserDAO {
-	
-	int insertUser(UserDTO userDTO);
 
 }
--- src/main/java/com/spring/service/UserDAOImpl.java
+++ src/main/java/com/spring/service/UserDAOImpl.java
@@ -4,18 +4,9 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.spring.model.UserDTO;
-
 @Repository
 public class UserDAOImpl implements UserDAO{
-	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
-
-	@Override
-	public int insertUser(UserDTO userDTO) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
 }
