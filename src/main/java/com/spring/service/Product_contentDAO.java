62216c02217ace46c7d87aecb6c1e95c884d4014
kimkyungyoung <kimkyungyoung@218.38.137.28>
2022-12-01 16:00:44 +0900

    first commit ProductDAO, ProductDAOImpl

--- src/main/java/com/spring/service/Product_contentDAO.java
+++ src/main/java/com/spring/service/Product_contentDAO.java
@@ -1,5 +1,12 @@
 package com.spring.service;
 
-public interface Product_contentDAO {
+import java.util.List;
 
+import com.spring.model.Product_contentDTO;
+
+public interface Product_contentDAO {
+	List<Product_contentDTO> getProductContList();
+	
+	Product_contentDTO getProduct(int product_no);
+	
 }
