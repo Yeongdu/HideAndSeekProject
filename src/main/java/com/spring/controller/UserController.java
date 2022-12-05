c890e762c302f903dbaa3d6a314746954a3bcea5
yeajizy <116059892+yeajizy@users.noreply.github.com>
2022-12-05 19:48:38 +0900

    221205_ 성인인증 및 회원가입, 로그인 진행 중

--- src/main/java/com/spring/controller/UserController.java
+++ src/main/java/com/spring/controller/UserController.java
@@ -1,46 +1,57 @@
 package com.spring.controller;
 
+import java.util.HashMap;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.model.UserDTO;
 import com.spring.service.UserDAO;
 
 @Controller
+@RequestMapping
 public class UserController {
-	
+
 	@Autowired
 	private UserDAO dao;
+
 	
-	@RequestMapping("user_login.do")
-	public String loginList() {
-		
-	return "user/login";
-		
-	}
-	
+	  @RequestMapping("user_login.do") 
+	  public String loginList() {
+	  
+	  return "user/login";
+	  
+	  }
+	 
+
 	@RequestMapping("join_form.do")
 	public String joinformList() {
-		
+
 		return "user/join";
 	}
-	
+
 	@RequestMapping("policy.do")
 	public String joinpolicyList() {
-		
+
 		return "user/policy";
 	}
-	
+
 	@RequestMapping("privacy.do")
 	public String joinprivacyList() {
-		
+
 		return "user/privacy";
 	}
 	
 	
 
+	
 }
