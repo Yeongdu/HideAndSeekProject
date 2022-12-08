d6fefcd7d62f39b24aee909f30a79e413395528a
leehyunji1107 <waffles03@naver.com>
2022-12-08 19:38:35 +0900

    221208 // 마이페이지 주문내역 ajax 구현 중

--- src/main/java/com/spring/service/MyPageDAO.java
+++ src/main/java/com/spring/service/MyPageDAO.java
@@ -1,9 +1,10 @@
 package com.spring.service;
 
 import java.util.List;
+import java.util.Map;
 
-import com.spring.model.CartDTO;
 import com.spring.model.DeliveryDTO;
+import com.spring.model.OrderDTO;
 import com.spring.model.ReviewDTO;
 import com.spring.model.Subscribe_userDTO;
 import com.spring.model.UserDTO;
@@ -11,9 +12,16 @@ import com.spring.model.UserDTO;
 public interface MyPageDAO {
 	
 	UserDTO getUserCont(String userId);
-	CartDTO getOrderCont(String userId);
-	ReviewDTO getReviewCont(String userId);
+	
+	List<OrderDTO> getOrderCont(Map<String, Object> map);
+	int getOrderCount(String userId);
+	
+	List<ReviewDTO> getReviewCont(String userId);
+	int getReviewCount(String userId);
+	
 	List<Subscribe_userDTO> getSubCont(String userId);
-	DeliveryDTO getDeliveryCont(String userId);
+	
+	List<DeliveryDTO> getDeliveryCont(String userId);
+	int getDeliveryCount(String userId);
 
 }
