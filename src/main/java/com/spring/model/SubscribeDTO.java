096f7560b783803b1775701e5fabb506a6612e80
mo11 <kgkt205@gmail.com>
2022-12-26 12:51:47 +0900

    221226 점심커밋 관리자 구독등록 하는 중

--- src/main/java/com/spring/model/SubscribeDTO.java
+++ src/main/java/com/spring/model/SubscribeDTO.java
@@ -11,14 +11,12 @@ public class SubscribeDTO {
 	private int sub_no; // 구독 번호
 	private String sub_package; // 구독 패키지명
 	
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private Date sub_date; // 판매 기간
+	private String sub_date; // 판매 기간
 	
 	private int product_no; // 상품 번호
 	private String sub_image; // 패키지 이미지
 	
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private Date sub_enddate;
+	private String sub_enddate;
 	
 	private String sub_status;
 	private int sub_package_no;
